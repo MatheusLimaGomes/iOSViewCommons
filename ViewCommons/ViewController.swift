450eb9df9d8c2689a0350700c68f0e93dbddb827
Matheus Gomes <matheus.gomes@altran-consultoria.com.br>
2020-07-01 12:08:00 -0300

    Initial Commit

---
+++ ViewCommons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewCommons
+//
+//  Created by Matheus Lima Altran on 01/07/20.
+//  Copyright © 2020 Matheus Gomes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
